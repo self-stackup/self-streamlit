7a91e390a2816eacecf9512295f02939dfb6f950
self-stackup <129053687+self-stackup@users.noreply.github.com>
2023-05-16 06:17:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -49,53 +49,6 @@ options_acc_area = ['Other', 'Office areas', 'Residential areas', ' Church areas
 features = ['Number_of_vehicles_involved','Number_of_casualties','Hour_of_Day','Type_of_collision','Age_band_of_driver','Sex_of_driver',
     'Educational_level','Service_year_of_vehicle','Day_of_week','Area_accident_occured']
 
-# Give a title to web app using html syntax
-st.markdown("<h1 style='text-align: center;'>Accident Severity Prediction App 🚧</h1>", unsafe_allow_html=True)
-
-# define a main() function to take inputs from user in form based approach
-def main():
-    with st.form("road_traffic_severity_form"):
-       st.subheader("Please enter the following inputs:")
-        
-       No_vehicles = st.slider("Number of vehicles involved:",1,7, value=0, format="%d")
-       No_casualties = st.slider("Number of casualties:",1,8, value=0, format="%d")
-       Hour = st.slider("Hour of the day:", 0, 23, value=0, format="%d")
-       collision = st.selectbox("Type of collision:",options=options_types_collision)
-       Age_band = st.selectbox("Driver age group?:", options=options_age)
-       Sex = st.selectbox("Sex of the driver:", options=options_sex)
-       Education = st.selectbox("Education of driver:",options=options_education_level)
-       service_vehicle = st.selectbox("Service year of vehicle:", options=options_services_year)
-       Day_week = st.selectbox("Day of the week:", options=options_day)
-       Accident_area = st.selectbox("Area of accident:", options=options_acc_area)
-        
-       submit = st.form_submit_button("Predict")
-
-# encode using ordinal encoder and predict
-    if submit:
-       input_array = np.array([collision,
-                  Age_band,Sex,Education,service_vehicle,
-                  Day_week,Accident_area], ndmin=2)
-        
-       encoded_arr = list(encoder.transform(input_array).ravel())
-        
-       num_arr = [No_vehicles,No_casualties,Hour]
-       pred_arr = np.array(num_arr + encoded_arr).reshape(1,-1)        
-      
-# predict the target from all the input features
-       prediction = model.predict(pred_arr)
-        
-       if prediction == 0:
-           st.write(f"The severity prediction is fatal injury⚠")
-       elif prediction == 1:
-           st.write(f"The severity prediction is serious injury")
-       else:
-           st.write(f"The severity prediction is slight injury")
-        
-       st.write("Developed By: Avi kumar Talaviya")
-       st.markdown("""Reach out to me on: [Twitter](https://twitter.com/avikumart_) |
-       [Linkedin](https://www.linkedin.com/in/avi-kumar-talaviya-739153147/) |
-       [Kaggle](https://www.kaggle.com/avikumart) 
-       """)
 
 
 # Give a title to web app using html syntax
